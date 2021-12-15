9bb9563bd806e1cdbf8cfc516bd8e09e498cc365
RDP77 <ravidwiputra@gmail.com>
2021-12-16 00:20:13 +0700

    Update

--- 3DESEncrypt/Form1.cs
+++ 3DESEncrypt/Form1.cs
@@ -7,11 +7,15 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace _3DESEncrypt
 {
     public partial class Form1 : Form
     {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern Int32 SendMessage(IntPtr hWnd, int msg, int wParam, [MarshalAs(UnmanagedType.LPWStr)] string iParam);
+        private const int EM_SETCUEBANNER = 0x1501;        
         public Form1()
         {
             InitializeComponent();
@@ -19,9 +23,43 @@ namespace _3DESEncrypt
 
         private void button1_Click(object sender, EventArgs e)
         {
+            openFileDialog1.FileName = "";
             if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
                 textBox1.Text = openFileDialog1.FileName;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          SendMessage(textBox2.Handle, EM_SETCUEBANNER, 0, "Key");        
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {            
+            try
+            {
+                TripleDES tDES = new TripleDES(textBox2.Text);
+                tDES.EncryptFile(textBox1.Text);
+                GC.Collect();
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                TripleDES tDES = new TripleDES(textBox2.Text);
+                tDES.DecryptFile(textBox1.Text);
+                GC.Collect();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
             }
         }
     }
---
+++ 3DESEncrypt/TripleDES.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace _3DESEncrypt
+{
+    public class TripleDES
+    {
+        private TripleDESCryptoServiceProvider des = new TripleDESCryptoServiceProvider();
+
+        public TripleDES(string key)
+        {
+            des.Key = UTF8Encoding.UTF8.GetBytes(key);
+            des.Mode = CipherMode.ECB;
+            des.Padding = PaddingMode.PKCS7;            
+        }
+
+        public void EncryptFile(string filepath)
+        {
+            byte[] Bytes = File.ReadAllBytes(filepath);
+            byte[] eBytes = des.CreateEncryptor().TransformFinalBlock(Bytes, 0, Bytes.Length);
+            File.WriteAllBytes(filepath, eBytes);
+        }
+
+        public void DecryptFile(string filepath)
+        {
+            byte[] Bytes = File.ReadAllBytes(filepath);
+            byte[] dBytes = des.CreateDecryptor().TransformFinalBlock(Bytes, 0, Bytes.Length);
+            File.WriteAllBytes(filepath, dBytes);
+        }
+    }
+}
